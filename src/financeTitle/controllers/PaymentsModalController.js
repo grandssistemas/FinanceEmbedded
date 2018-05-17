f57545350ae783ebae5cb8c3eeeb6b5b9bed82d7
Kolecha <lkolecha@hotmail.com>
2018-05-17 16:56:58 -0300

    alteração tela de recebimento de titulo adequação para novo fluxo de recebimento

--- src/financeTitle/controllers/PaymentsModalController.js
+++ src/financeTitle/controllers/PaymentsModalController.js
@@ -1,32 +1,100 @@
 PaymentsModalController.$inject = ['$scope',
     '$uibModalInstance',
-    'payments'
+    'PaymentService',
+    'gumgaController',
+    'parcelPayments'
 ];
 
 function PaymentsModalController($scope,
                                  $uibModalInstance,
-                                 payments) {
+                                 PaymentService,
+                                 gumgaController,
+                                 parcelPayments) {
 
-    $scope.payments = angular.copy(payments);
+    gumgaController.createRestMethods($scope, PaymentService, 'payment');
 
-    $scope.payments.forEach(payment => {
-        if (payment.value < 0){
-            payment.value = payment.value * -1;
-        }
-    });
+    $scope.parcelPayments = angular.copy(parcelPayments);
 
+    const getPayments = () => {
+        const arr = [];
+        $scope.parcelPayments.forEach((parcelPayment) => {
+            var gQuery = new GQuery(new Criteria('pp.id', ComparisonOperator.EQUAL, parcelPayment.id))
+                .join(new Join('obj.parcelPayments as pp', JoinType.INNER));
+            arr.push($scope.payment.methods.asyncSearchWithGQuery(gQuery));
+        });
+        return Promise.all(arr).then((response) => {
+            let toReturn = [];
+            response.forEach((data) => data.forEach((payment) => {
+                if (toReturn.filter((d) => angular.equals(d, payment)).length == 0) {
+                    toReturn.push(payment);
+                }
+            }));
+            return toReturn;
+        })
+    }
 
-    $scope.tableConfig = {
-        columns: 'name,date,value',
+    const initialize = () => {
+        $scope.messageLoading = 'Buscando pagamentos...';
+        getPayments().then((payments) => {
+            $scope.payments = payments;
+            delete $scope.messageLoading;
+        });
+    }
+
+    initialize();
+
+    $scope.searchEntrys = (payment) => {
+        PaymentService.getById(payment.id).then((paymentResponse) => {
+            $scope.paymentDetails = paymentResponse.data;
+
+            var newArr = [];
+            $scope.parcelsPayment = [{}];
+            angular.forEach($scope.paymentDetails.parcelPayments, function (value, key) {
+                var exists = false;
+                angular.forEach(newArr, function (val2, key) {
+                    if (angular.equals(value.parcel.number, val2.parcel.number)) { exists = true };
+                });
+                if (exists == false && value.parcel.number != "") { newArr.push(value); }
+            });
+            newArr = newArr.reverse();
+            $scope.parcelsPayment = newArr.map((data) => {
+                return {
+                    number: data.parcel.number
+                }
+            });
+
+        });
+    }
+
+    $scope.back = () => {
+        delete $scope.paymentDetails;
+    }
+
+    $scope.tableEntryConfig = {
+        columns: 'name, value',
         selection: 'single',
         materialTheme: true,
         columnsConfig: [
             {
                 name: 'name',
-                size: 'col-md-4',
+                size: 'col-md-2',
                 title: '<strong >Unidade Financeira</strong>',
                 content: '{{$value.financeUnit.name}}'
             },
+            {
+                name: 'value',
+                size: 'col-md-2',
+                title: '<strong>Valor</strong>',
+                content: '<span>{{$value.value | currency}}</span>'
+            }
+        ]
+    }
+
+    $scope.tableConfig = {
+        columns: 'date,value',
+        selection: 'single',
+        materialTheme: true,
+        columnsConfig: [
             {
                 name: 'date',
                 size: 'col-md-2',
