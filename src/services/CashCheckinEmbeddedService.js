580a6373c12826f8c719c430ef212bf7d003c41e
Felipe Sabadini Facina <fsabadini@hotmail.com>
2018-12-21 10:56:09 -0200

    Permitir abrir caixa para varias pessoas

--- src/services/CashCheckinEmbeddedService.js
+++ src/services/CashCheckinEmbeddedService.js
@@ -1,6 +1,6 @@
-CashCheckinEmbeddedService.$inject = ['GumgaRest', 'FinanceEmbeddedService'];
+CashCheckinEmbeddedService.$inject = ['GumgaRest', 'FinanceEmbeddedService', '$http'];
 
-function CashCheckinEmbeddedService(GumgaRest, FinanceEmbeddedService){
+function CashCheckinEmbeddedService(GumgaRest, FinanceEmbeddedService, $http){
     let service = new GumgaRest(FinanceEmbeddedService.getDefaultConfiguration().api + '/cashcheckin');
 
     service.getCurrentCheckin = function () {
@@ -10,6 +10,26 @@ function CashCheckinEmbeddedService(GumgaRest, FinanceEmbeddedService){
     service.getByCurrentCashCheckin = function (date) {
         return service.extend('get', '/getbycurrentcashcheckin?date='+date);
     };
+
+    service.update = function (entity) {
+        if(entity.cashCheckouts.length > 0) {
+            return $http.post(FinanceEmbeddedService.getDefaultConfiguration().api + '/cashcheckin', entity);
+        }
+        const openCash = {
+            "employee": {
+                "id": entity.employee.id,
+                "securityLogin": entity.employee.employeeValue.securityLogin
+            },
+            "cashRegister": {
+                "id": entity.group.id
+            },
+            "pointOfSale": {
+                "id": entity.group.integrationValue.integrationId,
+                "change": entity.change
+            }
+        }
+        return $http.post('http://127.0.0.1:8084/finance-api/api/cashcheckin/do-cashcheckin', openCash);
+    }
 
     return service;
 }
