b05175c5f74a0cfb982865431188e634054bd938
Gumga IT <info.mateusmiranda@gmail.com>
2019-10-08 09:23:00 -0300

    merge

--- src/cashcheckout/controllers/CashCheckoutEmbeddedFormController.js
+++ src/cashcheckout/controllers/CashCheckoutEmbeddedFormController.js
@@ -122,41 +122,45 @@ function CashCheckoutEmbeddedFormController(
 					},
 					(isConfirm) => {
 						if (isConfirm) {
-							const variables = [];
-							GenericReportService.getDefault('CASHCHECKOUT').then((response) => {
-								if (response.data) {
-									CompanyService.variablesReport().then((vari) => {
-										const variables = vari;
-										const filters = '';
-										variables.push(FinanceReportService.mountVariable('', 'idpdv', cashier.group.id));
-										variables.push(FinanceReportService.mountVariable('', 'idcheckin', cashier.id));
-										const modalInstance = $uibModal.open({
-											animation: $scope.animationsEnabled,
-											templateUrl: viewModal,
-											controller: 'ViewerController',
-											windowClass: 'full-modal',
-											backdrop: 'static',
-											size: 'lg',
-											resolve: {
-												entity() {
-													return response.data;
-												},
-												filters() {
-													return filters;
-												},
-												variable() {
-													return variables;
-												},
-												backState() {
-													return '';
+							$timeout(() => {
+								const variables = [];
+								GenericReportService.getDefault('CASHCHECKOUT').then((response) => {
+									if (response.data) {
+										CompanyService.variablesReport().then((vari) => {
+											const variables = vari;
+											const filters = '';
+											variables.push(FinanceReportService.mountVariable('', 'idpdv', cashier.group.id));
+											variables.push(FinanceReportService.mountVariable('', 'idcheckin', cashier.id));
+											const modalInstance = $uibModal.open({
+												animation: $scope.animationsEnabled,
+												templateUrl: viewModal,
+												controller: 'ViewerController',
+												type: 'RELATORIOS',
+												windowClass: 'full-modal',
+												backdrop: 'static',
+												size: 'lg',
+												resolve: {
+													entity() {
+														return response.data;
+													},
+													filters() {
+														return filters;
+													},
+													variable() {
+														return variables;
+													},
+													backState() {
+														return '';
+													}
 												}
-											}
+											});
 										});
-									});
-								} else {
-									SweetAlert.swal('Falta de Relatório de Fechamento de Caixa', 'Você esta sem o relatório de fechamento de caixa, contate o suporte.', 'warning');
-								}
-							});
+									} else {
+										SweetAlert.swal('Falta de Relatório de Fechamento de Caixa', 'Você esta sem o relatório de fechamento de caixa, contate o suporte.', 'warning');
+									}
+								});
+							})
+
 						}
 					}
 				);
